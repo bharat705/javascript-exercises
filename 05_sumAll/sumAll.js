9d0cd84cd89e24a8e0eeab263d0b4b625f1be326
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-08-29 21:11:45 +0545

    Solve 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    
+    let sum = 0;
 
+    if (!(int1.isNumber() || int2.isNumber())) {
+
+        return "ERROR";
+
+    } else if (!(int1.isInteger() || int2.isInteger())) {
+
+        return "ERROR";
+
+    } else if (int1 < 0 || int2 < 0) {
+
+        return "ERROR";
+
+    } else if (int1>int2) {
+
+        for (i=int2; i<=int1; i++) {
+            sum +=i;
+        }
+        return sum;
+
+    } else {
+
+        for (i=int1; i<=int2; i++) {
+            sum +=i;
+        }
+        return sum;
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
