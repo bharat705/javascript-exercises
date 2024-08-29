69952d7d3beb12b9fc74e2c31f42c4c91ca6182b
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-08-29 16:02:44 +0545

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    let newString = ""
+    if (num<0) {
+        return 'ERROR';
+    } else if (num === 0) {
+        return newString;
+    } else {
+        for(i=0; i<num; i++) {
+            newString += string;
+            if (i===num-1) {
+                return newString;
+            }
+        }
+    }
 };
 
 // Do not edit below this line
