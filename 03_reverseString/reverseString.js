4db0fc90b6cc1e27286684da561afdd9f9e99721
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-08-29 16:41:05 +0545

    Solve 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const wordsInString = [];
+    for (i=0; i<string.length; i++) {
+        wordsInString.unshift(string[i]);
+    }
+    return wordsInString.join("")
 };
 
 // Do not edit below this line
