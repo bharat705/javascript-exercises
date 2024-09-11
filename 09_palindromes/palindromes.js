68c8d3d3604fd0226703124505855ae95c42db25
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-09-11 14:01:07 +0545

    Solve exercise 08, 09, and 10. Changes to be committed: 	modified:   08_calculator/calculator.js 	modified:   08_calculator/calculator.spec.js 	modified:   09_palindromes/palindromes.js 	modified:   09_palindromes/palindromes.spec.js 	modified:   10_fibonacci/fibonacci.js 	modified:   10_fibonacci/fibonacci.spec.js 	modified:   package-lock.json

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  rawStr = removePunctuation(str).toLowerCase();
+  if (rawStr === reverseString(rawStr)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+function removePunctuation(str) {
+  return str
+    .split("")
+    .filter((char) => {
+      return /[a-zA-z0-9]/.test(char);
+    })
+    .join("");
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  const series = [1, 1];
+  if (num < 0) {
+    return "OOPS";
+  } else if (num == 0) {
+    return 0;
+  } else if (num <= series.length) {
+    return series[num - 1];
+  } else {
+    for (i = series.length; i < num; i++) {
+      series[i] = series[i - 1] + series[i - 2];
+    }
+    return series[num - 1];
+  }
 };
 
 // Do not edit below this line
