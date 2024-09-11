edbb725ad37b050298e05650bd74e0d32fe6cfe2
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-09-11 16:52:30 +0545

    Solve Exercise 12 - Find the Oldest Changes to be committed: 	modified:   12_findTheOldest/findTheOldest.js 	modified:   12_findTheOldest/findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArray) {
+  let maximumAge = 0;
+  let age = 0;
+  let oldestPerson = {};
+  peopleArray.forEach((person) => {
+    age = calculateAge(person);
+    if (age > maximumAge) {
+      maximumAge = age;
+      oldestPerson = person;
+    }
+  });
+  return oldestPerson;
 };
+
+function calculateAge(objects) {
+  const today = new Date().getFullYear();
+  const hasYearOfDeath = "yearOfDeath" in objects;
+  if (hasYearOfDeath) {
+    return (age = objects.yearOfDeath - objects.yearOfBirth);
+  } else {
+    return (age = today - objects.yearOfBirth);
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
